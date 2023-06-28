b0ee157cc696fd0feba6b730fbfc2afc5b277510
beease <beease.gestion@gmail.com>
2023-06-28 14:30:09 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* global chrome */
+import './App.scss';
+import { Login, Logout } from './utils/Auth/Auth';
+import AppLogged from './AppLogged'
+import { useQuery } from 'react-query';
+import React, { useEffect, useState } from 'react';
+import Logo from './assets/google.png'; 
+import { Switch } from './components/ui/switch';
+import { ColorPickerPopup } from './components/ui/colorPicker2';
+
+function Loading() {
+  return (
+      <div>loading google</div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [CurrentUserId, setCurrentUserId] = useState('');
+  const [loadingGoogle, setloadingGoogle] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+  // useEffect(() => {
+  //   chrome.storage.local.get(['userId'], function(result) {
+  //     if (!chrome.runtime.lastError) {
+  //       setCurrentUserId(result.userId)
+  //     }
+  //   });
+  // }, []);
+
+
+  // const handleLogout = async () => {
+  //   setloadingGoogle(false)
+  //   await Logout();
+  //   setCurrentUserId('')
+  // };
+
+  // const handleLogin = async () => {
+  //   setloadingGoogle(true)
+  //   const logged = await Login();
+  //   setCurrentUserId(logged)
+  // };
+
+  const LoginButton = () => {
+    return (
+      <div id='app_logout'>
+        <button id='google_connect' onClick={() => {handleLogin()}}>
+            {
+              !loadingGoogle ?
+                <div className='google_connect_cont'>
+                  <img alt='google_logo' src={Logo} />
+                  <div id='google_connect_text' >Connexion avec google</div>
+                </div>
+              :
+              <div className='google_loading_cont'>                 
+                  <Loading/>
+                </div>
+            }
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      </div>)
+  }
+
+  const [currentOption, setCurrentOption] = useState<number>(0);
+  const [colorWorkSpace, setColorWorkSpace] = useState('');
+  const [colorWorkSpacePopup, setColorWorkSpacePopup] = useState(false);
+console.log(colorWorkSpace)
+  return (
+    <div id='app'>
+      <div style={{
+        display: 'flex',
+        width: '100vw',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        
+        <ColorPickerPopup
+              setColor={setColorWorkSpace}
+              colorPopup={colorWorkSpacePopup}
+              setColorPopup={setColorWorkSpacePopup}
+              color={colorWorkSpace || '#4969fb'}
+            />
+        {/* <Switch 
+          width={200}
+          height={48}
+          options={['En cours', 'Archives']}
+          color={'#4969fb'}
+          currentOption={currentOption}
+          setCurrentOption={setCurrentOption}
+        /> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/* {CurrentUserId ? (
+          <div>
+            <div>logged</div>
+            <div onClick={() => {handleLogout()}}>logout</div>
+          </div>
+      ) : (
+        <div id='app_logout'>
+           <LoginButton/>
+        </div>
+      )} */}
+    </div>
+  );
 }
 
 export default App
