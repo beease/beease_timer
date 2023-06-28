709564e277dec65b7dc2bd82fa33710934d415c5
beease <beease.gestion@gmail.com>
2023-06-28 14:39:31 +0200

    chantier 2

---
+++ client/src/AppLogged.tsx
@@ -0,0 +1,12 @@
+/* global chrome */
+import React, { useState } from 'react';
+
+const AppLogged = () =>{
+    return (
+        <div id='app_login'>
+           logged
+        </div>
+    )
+}
+
+export default AppLogged;
