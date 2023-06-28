eba6bc87a7543a87befcc6888ba614779317df8a
beease <martinlotz.pro@gmail.com>
2023-06-28 14:26:38 +0200

    add server

---
+++ server/router.ts
@@ -0,0 +1,19 @@
+/*import express, { Request, Response, NextFunction } from 'express';
+import { PrismaClient, Role } from '@prisma/client';
+import { VERIFY_TOKEN } from './services/auth';
+const prisma = new PrismaClient();
+import {checkRoleMiddleware , handleAsyncError} from './services/router/utils'
+const router = express.Router();
+
+// Add custom endpoints
+
+// router.get(
+//   `/user/me`,
+//   checkRoleMiddleware ([]), 
+//   async (res: Response) => {
+//      // @ts-ignore
+//    await handleAsyncError(res, () => prismaModel.findMany());
+//  });
+
+
+export default router;*/
