24668f59448d97d9c391199250bf67dfe54f2913
beease <beease.gestion@gmail.com>
2023-07-06 16:39:41 +0200

    adding workspace endpoints create & get

--- server/routers/workspaceRouter.ts
+++ server/routers/workspaceRouter.ts
@@ -1,7 +1,14 @@
 import { z } from 'zod';
 import { router, publicProcedure } from '../trpc';
-import { Prisma, PrismaClient } from '@prisma/client';
+import * as workspaceService from '../services/CRUD/workspaceService';
 
-const prisma = new PrismaClient();
+export const workspaceRouter = router({
+    createWorkspace: publicProcedure.input(z.object({ name: z.string(), color: z.string() })).mutation(async (opts) => {
+        return await workspaceService.createWorkspace(opts.input.name, opts.input.color);
+    }),
+    getWorkspaceById: publicProcedure.input(z.object({ id: z.string() })).query(async (opts) =>{
+        return await workspaceService.getWorkspaceById(opts.input.id);
+    })
+});
 
-export const workspaceRouter = router({});
+
---
+++ server/services/CRUD/workspaceService.ts
@@ -0,0 +1,53 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { fetchGoogleUserInfo } from "../google/googleApis";
+import { asyncFunctionErrorCatcher } from "../utils/errorHandler";
+import { signJwt } from "../auth/jwt";
+const prisma = new PrismaClient();
+
+export const createWorkspace = async (name:string, color:string) => {
+
+    return asyncFunctionErrorCatcher(
+        () => prisma.workspace.create({
+            data: ({
+                createdAt: new Date(),
+                name: name,
+                color: color,
+            })
+        })
+    )
+
+};
+
+export const getWorkspaceById = async (id:string) => {
+    return asyncFunctionErrorCatcher(
+        () =>
+        prisma.workspace.findUnique({
+            where: {
+                id: id,
+            },
+            select: {
+                id: true,
+                name: true,
+                createdAt: true,
+                color: true,     
+                projects: true           
+            },
+        }),
+        
+    );
+};
+
+
+export const updateWorkspace = async () => {}
+
+export const deleteWorkspace = async (id: string) => {
+    return asyncFunctionErrorCatcher(
+        () =>
+        prisma.workspace.delete({
+            where: {
+             id,
+            },
+        }),
+        "Failed to delete user by id with Prisma."
+    );
+}
